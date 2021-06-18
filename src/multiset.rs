9ebf48788fea3e0a42842d78ee99ab49f91d1b65
Shahar Yarnitzky <shahar.yarn@gmail.com>
2021-06-18 17:24:39 +0300

    Implement add for multisets

--- src/multiset.rs
+++ src/multiset.rs
@@ -1,6 +1,7 @@
 use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::hash::Hash;
+use std::ops::{Add, Sub};
 
 pub struct MultiSet<T> {
     map: HashMap<T, i32>,
@@ -14,7 +15,11 @@ impl<T: Eq + Hash> MultiSet<T> {
     }
 
     pub fn insert(&mut self, item: T) -> Option<i32> {
-        let new_count = self.get(&item).unwrap_or(0) + 1;
+        self.repeat_insert(item, 1)
+    }
+
+    pub fn repeat_insert(&mut self, item: T, count: i32) -> Option<i32> {
+        let new_count = self.get(&item).unwrap_or(0) + count;
         self.map.insert(item, new_count)
     }
 
@@ -65,6 +70,25 @@ impl<T: Eq + Hash> PartialOrd for MultiSet<T> {
             return Some(Ordering::Greater);
         };
         None
+    }
+}
+
+impl<T: Eq + Hash> Add for MultiSet<T> {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        let mut new_map: MultiSet<T> = MultiSet::new();
+        other.map.into_iter().for_each(|(key, value)| {new_map.repeat_insert(key, value);});
+        self.map.into_iter().for_each(|(key, value)| {new_map.repeat_insert(key, value);});
+        new_map
+    }
+}
+
+impl<T: Eq + Hash + Clone> Clone for MultiSet<T> {
+    fn clone(&self) -> Self {
+        let mut new_map: MultiSet<T> = MultiSet::new();
+        self.map.clone().into_iter().for_each(|(key, value)| {new_map.repeat_insert(key, value);});
+        new_map
     }
 }
 
@@ -159,5 +183,19 @@ mod tests {
         assert!(set > other);
         other.insert(2);
         assert!(!(set > other) && !(set < other));
+    }
+
+    #[test]
+    fn add_groups() {
+        let mut set: MultiSet<i32> = MultiSet::new();
+        set.insert(5);
+        set.insert(3);
+        set.insert(5);
+        let mut other: MultiSet<i32> = MultiSet::new();
+        other.insert(5);
+        other.insert(2);
+        let added_set = set.clone() + other.clone();
+        assert_eq!(added_set.get(&5).unwrap(), 3);
+        assert_eq!(set.get(&5).unwrap(), 2);
     }
 }
